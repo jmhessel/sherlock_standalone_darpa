fa1fc62d7b332fc8a232664f4a70b24f3505e77b
Jack Hessel <jackh@allenai.org>
2021-12-08 12:32:02 -0800

    more work

---
+++ download_model_and_data.py
@@ -0,0 +1,40 @@
+'''
+This script downloads the best model and the data needed to run the analytic.
+
+python download_model_and_data.py
+'''
+import argparse
+import subprocess
+import os
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    return parser.parse_args()
+
+
+def call(x):
+    subprocess.call(x, shell=True)
+    
+
+def main():
+    args = parse_args()
+
+    for url in [
+            'https://storage.googleapis.com/ai2-jack-public/sherlock_darpa_selfeval/RN50x16_best_checkpoint_inference_only.pt',
+            'https://storage.googleapis.com/ai2-jack-public/sherlock_darpa_selfeval/im2txt_annotations_all_v2.zip',
+            'https://storage.googleapis.com/ai2-jack-public/sherlock_darpa_selfeval/self_eval_images.zip',
+            'https://storage.googleapis.com/ai2-jack-public/sherlock_darpa_selfeval/val_instances_darpa_self_eval.json.zip']:
+        if not os.path.exists(url.split('/')[-1]):
+            call('wget {}'.format(url))
+
+    if not os.path.exists('im2txt_annotations_all_v2.json'):
+        call('unzip im2txt_annotations_all_v2.json.zip')
+    if not os.path.exists('self_eval_images/VG_100K_2/2412277.jpg'):
+        call('unzip self_eval_images.zip')
+    if not os.path.exists('val_instances_darpa_self_eval.json'):
+        call('unzip val_instances_darpa_self_eval.json.zip')
+    
+    
+if __name__ == '__main__':
+    main()
